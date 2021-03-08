c7a2255d79e48e73d063492c2496fcb6803b0e7d
alelaru <alelaru@gmail.com>
2021-03-08 13:49:05 +0100

    First structure of the React Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import Cards from './components/Cards/Cards';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import Chart from "./components/Chart/Chart.jsx"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+        <Cards></Cards>
+        <CountryPicker></CountryPicker>
+        <Chart></Chart>
     </div>
   );
 }
